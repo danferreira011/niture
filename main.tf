2e38dc0818ab63ef939e9ce17ae952b63330d489
Danilo Ferreira <danilosantos.turbo@gmail.com>
2026-01-05 19:52:35 -0300

    voltando ao original

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ variable "fingerprint" {}
 variable "private_key" {}
 variable "ssh_public_key" {}
 
-provider "oracle/oci" {
+provider "oci" {
   tenancy_ocid = var.tenancy_ocid
   user_ocid = var.user_ocid
   fingerprint = var.fingerprint
